2db81448a09e02d2b0ddcd4c700aea074dc8a7c7
goutam2018 <35404543+goutam2018@users.noreply.github.com>
2018-01-14 01:12:09 +0530

    i have added new bash file

---
+++ user.sh
@@ -0,0 +1,5 @@
+# Numbers of Users
+echo Currenctly Logged in users are ` who | wc -l`
+echo Currently Logged in users are ` who -q `
+echo Logged in Users Count is: ` users | wc -w `
+echo "Success"
